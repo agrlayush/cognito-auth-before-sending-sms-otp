0983675843b730b6f1057626c3d866ef036855c9
Ayush Agrawal <ayushagl@amazon.com>
2024-10-07 12:25:13 +0530

    dynamodb returns

--- create-token-lambda.js
+++ create-token-lambda.js
@@ -1,23 +1,31 @@
+import { DynamoDB } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocument } from '@aws-sdk/lib-dynamodb';
 import crypto from 'crypto';
-
-const secret_salt = "your-random-salt"
+const dynamoDB = DynamoDBDocument.from(new DynamoDB());
 export const handler = async (event) => {
-    console.log(event)
-    var user_ip = event.headers['X-Forwarded-For']
-    var user_agent = event.headers['User-Agent']
-    var timestamp = Date.now()
-    var param = {
-        "secret_salt": secret_salt,
-        "user_ip": user_ip,
-        "user_agent":user_agent,
-        "timestamp": timestamp
-    }
-
-    var param_string = JSON.stringify(param);
-    const token=crypto.createHash('sha256').update(param_string).digest('hex');
-    return {
-            statusCode: 200,
-            body: JSON.stringify({ token: token, timestamp: timestamp })
+    const token = crypto.randomBytes(16).toString('hex');
+    const expirationTime = Math.floor(Date.now() / 1000) + 300; // 5 minutes from now
+    
+    const params = {
+        TableName: 'SingleUseTokens',
+        Item: {
+            token: token,
+            expirationTime: expirationTime,
+            used: false
+        }
     };
     
+    try {
+        await dynamoDB.put(params);
+        return {
+            statusCode: 200,
+            body: JSON.stringify({ token: token })
+        };
+    } catch (error) {
+        console.error('Error:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Error generating token' })
+        };
+    }
 };
