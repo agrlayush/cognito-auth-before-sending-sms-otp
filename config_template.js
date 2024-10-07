c082254f439babb8c52c19cf58293d8d5d52f18a
Ayush Agrawal <ayushagl@amazon.com>
2024-10-07 14:00:03 +0530

    config template added

---
+++ config_template.js
@@ -0,0 +1,7 @@
+// rename the file to config.js
+// replace the urls to valid urls from your implementation
+
+get_token_url = 'API gateway URL for the lambda function to create-token.'  //Deploy create-token-lambda.js
+user_pool_id = 'Cognito User Pool Id'
+cognito_client_id = 'Cognito Client Id' //fetch from App integration tab on cognito dashbaord
+authentication_validation_url = '(Optional)Any url that is authenticated using the same cognito user pool to validate the user authentication worked'
